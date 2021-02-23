902ab4bcb270cf00f1d960ce989b4c2f97fd45c6
inwooshin <60161818@mju.ac.kr>
2021-02-23 20:12:33 +0900

    객체 내장 함수 공부
    reverse 나 sort, append 와 같은 리스트 구조의 내장 메소드를 공부했다.
    그리고 배열에 대해서 자르고 pop 하는 내장함수에 대해서도 공부했다.

--- hello2.py
+++ hello2.py
@@ -65,5 +65,92 @@ print(test.count("째"))
 a = ",".join('abcd')
 print(a)
 
-a = 1
+print(a)
+
+a = 'abcd'
+print(a.upper())
+
+# 이렇게 할 경우에는 변수에 다른게 저장이 된다.
+a = "life is too short".replace("life", "Your dog")
+print(a)
+
+# 이렇게 할 경우는 변수에 저장되지는 않는다 그래서
+a = "life is too short"
+# a = 을 추가해주면 바뀐 것이 변수에 할당된다.
+a.replace("life", "Your dog");
+print(a)
+
+#default 짜르는 문자는 스페이스바, 그러나 다른걸로 변경가능
+print(a.split())
+b = a.split()
+a = "l.o.v.e"
+
+print(a.split('.'))
+
+#이렇게 분리된 리스트를 받아서 출력할 수 있다.
+print(b[:])
+
+#list
+a = ['신인우', '문채원', ['1', '2']]
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[2][0])
+print(a[2][1])
+
+a = [1,2,3]
+b = 4
+b += a[0]
+print(b)
+
+a = [1,2,3]
+b = [4,5,6]
+
+#이렇게 하면 이어붙이기다!
+print(a + b)
+
+a += b
+print(a)
+
+# 0이상 3 미만!!!! 까지 인거 유의!
+a[0:3] = [7,7,7]
+print(a)
+del a[0:3]
+print(a)
+
+#이거는 리스트의 주소를 넘기는 함수인가보다 리스트 변수가 바뀌어있다.
+a.append(8)
+a.append(8)
+a.append(8)
+print(a)
+
+a.reverse()
+print(a)
+
+a.sort()
+print(a)
+
+#첫번째로 8을 갖는 리스트 a 에 대한 인덱스를 반환한다.
+print(a.index(8))
+
+#0번째 인덱스에 4를 추가하자
+a.insert(0,4)
+print(a)
+
+a.remove(8)
+print(a)
+
+# pop 은 맨 뒤의 인덱스부터 제거해줌
+a.pop()
+print(a)
+
+a.pop()
+print(a)
+
+#4가 이 리스트에 몇개있는지
+print(a.count(4))
+
+#맨뒤에 추가
+a.extend([1,2,3,4])
 print(a)
