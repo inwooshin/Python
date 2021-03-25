67554a42dd038d3743f753c5146cf6547e790387
inwooshin <60161818@mju.ac.kr>
2021-03-25 09:10:27 +0900

    pyplot, 그래프의 정확도를 높임

--- pylab.py
+++ pylab.py
@@ -5,7 +5,7 @@ x = list(map(lambda i: round(i), np.arange(-2.1,2.1,step = 0.1)))
 y = list(map(lambda i: round(i), np.arange(-2.1,2.1,step = 0.1)))
 
 mini = 0.000001
-weight1, weight2, thres = (0.3375, 0.3375, 0.297)
+weight1, weight2, thres = (0.5, 0.6, 0.55)
 
 font1 = {'family': 'serif',
       'color':  'black',
@@ -13,7 +13,7 @@ font1 = {'family': 'serif',
       'size': 14}
 
 def perceptron(w1, w2, x1, x2):
-    if(w1 * x1 + w2 * x2 >= thres):
+    if(w1 * x1 + w2 * x2 > thres or abs(w1 * x1 + w2 * x2 - thres) < 0.000001):
         plt.plot(x1, x2, "co", markersize = 6, visible = True)
     else:
         plt.plot(x1, x2, "yo", markersize = 6, visible = True)
@@ -26,10 +26,9 @@ def line(x1, x2):
     plt.plot(x1,x2, "b-", label = (str(weight1) + ' * ' + \
         'x1 + ' + str(weight2) + ' * ' + 'x2 = ' + str(round(thres,1))), visible = True)
 
-
 # round ver
-x1 = list(map(lambda i: round((thres - weight2 * i) / weight1, 1), np.arange(-2.1,3.1,step = 0.1)))
-x2 = list(map(lambda i: round((thres - weight1 * i) / weight2, 1), x1))
+x1 = list(map(lambda i: round((thres - weight2 * i) / weight1, 4), np.arange(-3.1,3.1,step = 0.1)))
+x2 = list(map(lambda i: round((thres - weight1 * i) / weight2, 4), x1))
 
 ''' no round
 x1 = list(map(lambda i: (thres - weight2 * i) / weight1, np.arange(-2.1,2.1,step = 0.1)))
