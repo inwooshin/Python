01d69e71b311bd1766eacfd282fb1c4dd4713f75
inwooshin <60161818@mju.ac.kr>
2021-04-01 08:35:20 +0900

    error 값도 고려해서 출력
     - threshold 를 배제하고 y = w1 * x1 + w2 * x2 의 공식
     - 300번의 연산을 넘기면 while 문 break

--- pyplot_area.py
+++ pyplot_area.py
@@ -1,6 +1,6 @@
 
 
-w1, w2, thres = (0.1, 0.1, 0.3)
+w1, w2, thres = (8.0, 8.0, 8.0)
 n = 0.01
 t = [[0,1,1,1], [0,0,0,1]]
 xx = [[0,0], [0,1], [1,0], [1,1]]
@@ -10,22 +10,18 @@ wcount = 2
 
 def perceptron(t):
     global w1, w2, thres, wcount, n
-    check = 0 
+    check = 0
+    error = 0
     for i in range(0,4):
-        y = 0
-        if(w1 * xx[i][0] + w2 * xx[i][1] > thres or \
-            abs(w1 * xx[i][0] + w2 * xx[i][1] - thres) < 0.000001):
-            y = 1
-
-        if(y == t[i]):
-            check += 1
+        y = w1 * xx[i][0] + w2 * xx[i][1]
 
         w1 = w1 + (n * (t[i] - y) * xx[i][0])
         w2 = w2 + (n * (t[i] - y) * xx[i][1])
+        error += 0.5 * (t[i] - y) * (t[i] - y)
     
+    print("w1 = %lf, w2 = %lf, err = %lf"%(w1, w2, error))
     wcount += 1
-
-    return count
+    return check
 
 check = 0
 count = 0
@@ -33,7 +29,8 @@ while check != 4:
     check = 0
     check = perceptron(t[or_and])
     count += 1
-    if count >= 100:
+    if count >= 300:
+        print("> 1000")
         break
 
 print('%.3f * x1 + %.3f * x2 = %.3f'%(w1,w2,thres))
