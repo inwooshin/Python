1266e7e64826f67aeebbf5d24f51d88321171562
inwooshin <60161818@mju.ac.kr>
2021-05-18 11:34:11 +0900

    점을 찍어 plot 을 만들어주기
    머신러닝의 과제를 위해서는 한 영역이 필요하다. 그래서 그러한 영역으로 나눠서 코드를 가르쳐주기 위해서는
    점을 찍고 그 점들이 연결되는 것을 코드로 구현하였다.

---
+++ CreatePlot.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+ 
+fig, ax = plt.subplots(figsize=(15, 8))
+plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interactive Plot')
+ 
+ax.set(xlim=[0, 50], ylim=[0, 50])
+ax.set_aspect('auto', adjustable='box')
+ 
+xdata = []
+ydata = []
+line, = ax.plot(xdata, ydata)
+ 
+f = open("12_test.txt", "w")
+ 
+def add_point(event):
+    if event.inaxes != ax:
+        return
+ 
+    # button 1: 마우스 좌클릭
+    if event.button == 1:
+        x = event.xdata
+        y = event.ydata
+
+        a = str(round(x, 6)) + ' ' + str(round(y, 6)) + '\n'
+        print(a)
+        f.write(a)
+ 
+        xdata.append(x)
+        ydata.append(y)
+ 
+        line.set_data(xdata, ydata)
+        plt.draw()
+            
+    # button 3: 마우스 우클릭 시 기존 입력값 삭제
+    if event.button == 3:
+        xdata.pop()
+        ydata.pop()
+        line.set_data(xdata, ydata)
+        plt.draw()
+ 
+    # 마우스 중간버튼 클릭 시 종료하기
+    if event.button == 2:
+        f.close()
+ 
+cid = plt.connect('button_press_event', add_point)
+
+plt.show()
