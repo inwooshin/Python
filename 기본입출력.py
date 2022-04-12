2b8b1fd6da429c053e98359d7b8eaeb59e3cc048
inwooshin <dkrlehowl45@naver.com>
2022-04-13 00:07:24 +0900

    기본 입출력

---
+++ 기본입출력.py
@@ -0,0 +1,23 @@
+#n = int(input())
+#data = list(map(int, input().split()))
+
+# print(n, data)
+
+# #여기서는 굳이 리스트로 바꾸는 것이 아니라 맵으로 가능하다!
+# #but 숫자의 갯수가 딱 a,b,c 이렇게 세개에 맞게 해당해야한다
+# #a,b,c = map(int, input().split())
+# print(a,b,c)
+
+#ctrl + / 은 전부 주석!
+
+# import sys
+
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+# print(7, end = ' ')
+# print(7, end = ' ')
+
+#요런 f string 문법으로 변수를 걸어서 출력할 수 있다!
+name = '인우'
+print(f"안녕하세요 저는 {name}입니다")
